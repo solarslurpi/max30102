d6209a9c62849c2144cfa5ade89f91c7bda564bb
Happyday <happyday.mjohnson@gmail.com>
2020-12-27 05:31:18 -0800

    first commit

---
+++ adafruit_max30102.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2020 happyday
+#
+# SPDX-License-Identifier: MIT
+"""
+`adafruit_max30102`
+================================================================================
+
+CircuitPython helper library for the MAX30102 pulse oximetry and heart-rate monitor module.
+
+
+* Author(s): happyday
+
+
+"""
+
+# imports
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MAX30102.git"
