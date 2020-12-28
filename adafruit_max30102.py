a707484154611139efddfd46439ef88a9f4c254a
Happyday <happyday.mjohnson@gmail.com>
2020-12-28 06:37:27 -0800

    got working on CP

--- adafruit_max30102.py
+++ adafruit_max30102.py
@@ -48,23 +48,25 @@ LED_CONFIG = 0x09  # Pulse width and power of LEDs
 TEMP_INTG = 0x16  # Temperature value, whole number
 TEMP_FRAC = 0x17  # Temperature value, fraction
 REV_ID = 0xFE  # Part revision
-_PART_ID = 0xFF  # Part ID, normally 0x11
+_PART_ID = 0xFF  # Part ID, normally 11 for the 30100, 21 for the 30102
+_MAX30102_ID = 21
 
 _I2C_ADDRESS = 0x57  # I2C address of the MAX30100 device
 
 
 class MAX30102:
-    """Library for the MAX30102 pulse oximetry and
-      heart-rate monitor module
+    """Easy access to values in the MAX30102's registers
         """
     _part_id = ROUnaryStruct(_PART_ID, "B")
 
     def __init__(self, is2c_bus, address=_I2C_ADDRESS):
         self.i2c_device = i2c_device.I2CDevice(is2c_bus, address)
+        if self._part_id != _MAX30102_ID:
+            raise AttributeError("Cannot find a MAX30102")
 
     @property
     def part_id(self):
-        """The module's part id
+        """The part id of the MAX30102
 
         :return: The value of the part id stored in the module's PART ID register.
         :rtype: unsigned byte
--- examples/max30102_simpletest.py
+++ examples/max30102_simpletest.py
@@ -1,3 +1,28 @@
 # SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 #
 # SPDX-License-Identifier: Unlicense
+"""This is a very simple example for you to start using this library.
+    """
+# import sys
+import board
+import busio
+import adafruit_max30102
+
+i2c = busio.I2C(board.SCL, board.SDA)
+max30102 = adafruit_max30102.MAX30102(i2c)
+print(f'The part id is {max30102.part_id}')
+
+
+# def is_max30102():
+#     """Check if the MAX30102 is working as expected.  It will print out the
+#     error message set when the error occured.  This example does not use the
+#     function.  It is included to show a more robust way of
+#     :return: True or False
+#     :rtype: bool
+#     """
+#     try:
+#         adafruit_max30102.MAX30102(i2c)
+#     except AttributeError as error:
+#         print(error)
+#         return True
+#     return False
